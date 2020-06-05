5c1fd49ac393a277a43b07367ea49863bc828d05
“Nirvana” <nirvana.supermind@gmail.com>
2020-06-04 17:53:39 -0700

    Changed taylor

--- scripts/taylor.js
+++ scripts/taylor.js
@@ -8,11 +8,11 @@ define(["derivative", "fac"], function (derivative, fac) {
 
 
 
-                if (i >= 3) {
-                    result.push((derivative.derivativeStable(f, a, i) / (fac.fac(i + 1)))*(1/(i**i**(i/1.2))));
-                } else {
+                // if (i >= 3) {
+                //     result.push((derivative.derivativeStable(f, a, i) / (fac.fac(i + 1)))*(1/(i**i**(i/1.2))));
+                // } else {
                     result.push(derivative.derivative(f, a, i) / (fac.fac(i + 1)));
-                }
+                // }
 
 
             }
